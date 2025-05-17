7086369bbb38893e5c993e0b3d902a1d8e1e349e
bob7123 <git16@bob7.com>
2025-05-17 16:56:43 +0200

    Remove sensitive file

--- frontend-php/config/env.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Production environment configuration
-putenv('TRA_DB_HOST=localhost');
-putenv('TRA_DB_USER=tra@volanticsystems.nl');
-putenv('TRA_DB_PASS=Hack123!');
-putenv('TRA_DB_NAME=tenant_rights');
-putenv('APP_ENV=production');
-putenv('DEBUG_MODE=false'); 
